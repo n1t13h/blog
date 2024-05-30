7b249c3fd6f0bdceb7eb87593cec0240cbb846db
Nitish Patel <nitishp74@gmail.com>
2024-05-30 23:55:18 +0530

    chore: Update heroImage paths and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
+import pagefind from "astro-pagefind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://nitishpatel.github.io",
-  base: "astro-test",
-  trailingSlash: "never",
-  integrations: [mdx(), sitemap()],
+  base: "blog",
+  integrations: [mdx(), sitemap(), pagefind()],
 });
--- src/content/blog/dsa/arrays.md
+++ src/content/blog/dsa/arrays.md
@@ -2,7 +2,7 @@
 title: Arrays
 description: DSA Notes for Array
 pubDate: Jul 10 2022
-heroImage: /astro-test/blog-placeholder-3.jpg
+heroImage: /blog/blog-placeholder-3.jpg
 ---
 
 # DSA Array notes
